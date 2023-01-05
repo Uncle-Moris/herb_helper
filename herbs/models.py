011ea7bb1b3828e2baccb5cca08aeed5a2dccda7
maurycywoznica <mjw11@wp.pl>
2023-01-05 23:39:16 +0100

    Initial commit

---
+++ herbs/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+
+
+class Category(models.Model):
+    class Meta:
+        ordering = ('pk',)
+
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Country(models.Model):
+    class Meta:
+        ordering = ('name',)
+
+    name = models.CharField(unique=True)
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Flavor(models.Model):
+    class Meta:
+        ordering = ('name',)
+
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Herb(models.Model):
+    name = models.CharField(unique=True, max_length=255)
+    category = models.ForeignKey(Country, on_delete=models.SET_NULL)
+    country_of_origin = models.ManyToManyField(Country)
+    flavor = models.ForeignKey(Flavor, on_delete=models.CASCADE)
+    
