16fd529cc8e9a7856ad7ce599fa975082e15bf9f
maurycywoznica <mjw11@wp.pl>
2023-01-06 20:44:29 +0100

    basics views

--- herbs/views.py
+++ herbs/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from .models import Herb, Category, Country, Flavor
+
+
+class HerbListView(ListView):
+    model = Herb
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        pass
+
+
+class CategoryListView(ListView):
+    model = Category
+    pass
+
+
+class FlavorListView(ListView):
+    model = Flavor
+    pass
+
+
+class CountryListView(ListView):
+    model = Country
+    pass
