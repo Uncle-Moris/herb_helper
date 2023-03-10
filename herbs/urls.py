8b0aab32ecb880607276721ce87e8cd7ef6d4077
maurycywoznica <mjw11@wp.pl>
2023-03-10 23:25:35 +0100

    routing

--- herbs/urls.py
+++ herbs/urls.py
@@ -6,10 +6,10 @@ from .views import (
     HerbListView,
     HerbDetailsView
 )
-
+app_name = "herbs"
 
 urlpatterns = [
-    path('', HerbListView.as_view(), name="herb-list"),
+    path('list', HerbListView.as_view(), name="herb-list"),
     path('<slug:slug>', HerbDetailsView.as_view(), name="herb-detail-page"),
     path('<slug:slug>', FlavorListView.as_view()),
     path('<slug:slug>', CategoryListView.as_view()),
--- users/views.py
+++ users/views.py
@@ -1,22 +1,44 @@
 from django.shortcuts import render, redirect
 from .forms import UserRegisterForm
-from django.contrib.auth import login
+from django.contrib.auth import login,logout, authenticate
 from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
 
 
-def register(request):
+def user_register(request):
 	if request.method == "POST":
 		form = UserRegisterForm(request.POST)
 		if form.is_valid():
 			user = form.save()
 			login(request, user)
-			messages.success(request, "Registration successful." )
-			return redirect("main:homepage")
+			messages.success(request, "Registration successful.")
+			return redirect("herbs:herb-list")
 		messages.error(request, "Unsuccessful registration. Invalid information.")
 	form = UserRegisterForm()
-	return render(request=request, template_name="users/register.html", context={"form":form})
+	return render(request=request, template_name="users/register.html", context={"form": form})
 
-# class UserRegisterView(CreateView):
-#     template_name = "users/register.html"
-#     form_class = UserRegisterForm
-#     success_url = reverse_lazy('register')
+
+def user_login(request):
+	if request.method == "POST":
+		form = AuthenticationForm(request, data=request.POST)
+		if form.is_valid():
+			username = form.cleaned_data.get('username')
+			password = form.cleaned_data.get('password')
+			user = authenticate(username=username, password=password)
+			if user is not None:
+				login(request, user)
+				messages.info(request, f"You are now logged in as {username}.")
+				return redirect("herbs:herb-list")
+			else:
+				messages.error(request, "Invalid username or password.")
+		else:
+			messages.error(request, "Invalid username or password.")
+	form = AuthenticationForm()
+	return render(request=request, template_name="users/login.html", context={"form": form})
+
+
+def user_logout(request):
+	logout(request)
+	messages.info(request, "You have successfully logged out.")
+	return redirect("/herbs")
+
